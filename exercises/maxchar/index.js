21df9126639c470d6f613f5976fae17763fd3a8d
asuthamm <asuthamm@gmail.com>
2020-07-08 23:38:41 -0400

    done - maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charHash = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charHash[char]) {
+      charHash[char]++;
+    } else {
+      charHash[char] = 1;
+    }
+  }
+
+  for (let char in charHash) {
+    if (charHash[char] > max) {
+      max = charHash[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
