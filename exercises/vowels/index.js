1e6e49ec01fa7791db4d8d2b45b3d57cd2893dd8
asuthamm <asuthamm@gmail.com>
2020-07-15 22:25:36 -0400

    done - vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+//
+//   return count;
+// }
+
 
 module.exports = vowels;
