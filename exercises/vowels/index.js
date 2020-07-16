fc071635b3a75b43d8b7cbb509cd69850b079a19
asuthamm <asuthamm@gmail.com>
2020-07-15 23:22:19 -0400

    addtl solution to Vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -6,19 +6,25 @@
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
+// function vowels(str) {}
 
 // function vowels(str) {
-//   let count = 0;
-//   const checker = ['a', 'e', 'i', 'o', 'u'];
-//
-//   for (let char of str.toLowerCase()) {
-//     if (checker.includes(char)) {
-//       count++;
-//     }
-//   }
-//
-//   return count;
+//   const matches = str.match(/[aeiou]/gi);
+//   return matches ? matches.length : 0;
 // }
 
+function vowels(str) {
+  let count = 0;
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()) {
+    // if (checker.indexOf(char) >= 0) {
+    if (checker.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = vowels;
