419c0fd81f06a1f2d84ee69ba448b4a91eea7f69
asuthamm <asuthamm@gmail.com>
2020-07-25 13:38:58 -0400

    linkedlist - removeLast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -31,6 +31,51 @@ class LinkedList {
   getFirst() {
     return this.head;
   }
+
+  getLast() {
+    let node = this.head;
+    while (node.next) {
+      node = node.next;
+    }
+    return node;
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+    // if (this.getFirst() === this.getLast()) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+
+  insertLast(data) {
+    if (this.head === null) {
+      this.insertFirst(data)
+    } else {
+      let node = this.getLast();
+      node.next = new Node(data);
+    }
+  }
+
 }
 
 module.exports = { Node, LinkedList };
