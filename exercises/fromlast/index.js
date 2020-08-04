ae93d98800f9126abd9f851b1ee34802b863fa23
asuthamm <asuthamm@gmail.com>
2020-08-04 14:41:27 -0400

    done - fromlast

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -15,6 +15,16 @@ function fromLast(list, n) {
   let slow = list.getFirst();
   let fast = list.getFirst();
 
-  while (  n > 0 ) {}
+  while (  n > 0 ) {
+    fast = fast.next;
+    n--;
+  }
+  
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+}
 
 module.exports = fromLast;
