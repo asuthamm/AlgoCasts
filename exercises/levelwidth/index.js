4c475ba6f93fe52b88a32e1a769bcbb9bf2afcb6
asuthamm <asuthamm@gmail.com>
2020-08-06 23:48:39 -0400

    stuck @levelwidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,14 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const arr = [root, 's'];
+  const counters = [0];
+
+  while (arr.length > 1) {
+ 
+  }
+
+}
 
 module.exports = levelWidth;
