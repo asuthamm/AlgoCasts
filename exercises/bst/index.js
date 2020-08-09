a42b23ddb9fe57dd7d595775f1d52dd632e5dc47
asuthamm <asuthamm@gmail.com>
2020-08-09 02:56:32 -0400

    done - bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -19,7 +19,7 @@ class Node {
   }
   insert(data) {
     if (data < this.data && this.left) {
-      this.data.insert(data);
+      this.left.insert(data);
     } else if (data < this.data) {
       this.left = new Node(data);
     } else if (data > this.data && this.right) {
@@ -27,7 +27,18 @@ class Node {
     } else if (data > this.data) {
       this.right = new Node(data);
     }
+  }
 
+  contains(data) {
+    if (this.data === data) {
+      return this;
+    }
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+    }
+    return null;
   }
 }
 
